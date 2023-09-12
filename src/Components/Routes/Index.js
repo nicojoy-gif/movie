4ea0635f7e01cfe7b158b192dc2c913c22433ea6
Nicoratty <Nicoratty1@gmail.com>
2023-09-12 16:05:54 -0700

    CHANGE

--- src/Components/Routes/Index.js
+++ src/Components/Routes/Index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from '../Home'
-import MovieDetails from '../MovieDetails'
-import Movie from '../Movie'
+import Home from '../Home/Home'
+import MovieDetails from '../Home/MovieDetails'
+import Movie from '../Home/Movie'
 
 function Index() {
   return (
